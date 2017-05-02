01e376551480433b0dbd359d80894f6751acf714
Imad Ali <imadmali@gmail.com>
2017-05-01 21:09:43 -0400

    leftover commit

--- tests/testthat/test_class.R
+++ tests/testthat/test_class.R
@@ -1,14 +1,10 @@
 # test that functions return objects of the correct class
 
-context("azrmod")
+context("azrmod/azrsim")
 
-test_that("create_model returns an object of class 'azrmod'", {
+test_that("create_model returns an object of class 'azrmod', simulate returns an object of class 'azrsim'", {
   model <- create_model(system.file("examples/sho.txt", package="AZRsim"))
+  sho <- simulate(model,100)
   expect_equal(class(model), "azrmod")
+  expect_equal(class(sho), "azrsim")
 })
-
-# context("simulate")
-#
-# test_that("simulate returns an object of class 'azrsim", {
-#
-# })
