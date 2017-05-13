9e92414c6f24c73e3ef25899e5ce9dd017504566
Devin Pastoor <devin.pastoor@gmail.com>
2017-05-13 18:51:44 -0400

    function refactoring

--- diagramer.R
+++ diagramer.R
@@ -2,8 +2,46 @@ library(DiagrammeR)
 library(AZRsim)
 model <- create_model("modelPKtest.txt")
 model <- create_model("modelPK_3cpt_linear.txt")
-model_states <- AZRsim:::get_all_states(model)$stateODEs
 
+generate_diagram_syntax <- function(model) {
+  model_states <- AZRsim:::get_all_states(model)$stateODEs
+  cmpt_relations <- get_cmpt_relationships(model_states) %>%
+    mutate(result = paste0(from, "->", to, ";"))
+    paste0(cmpt_relations$result, collapse = " ")
+
+  test_grviz <- infuser::infuse("
+    digraph test {
+
+        # several 'node' statements
+        node [shape = box,fontname = Helvetica]
+        {{.model_states}}
+
+        node [shape = circle,
+        fixedsize = true,
+        width = 0.9]
+        {{.model_inputs}}
+
+        node [style=invis, width=0.1, height=0.1]
+        empty;
+
+        # inputs
+        {{.inputs}}
+        # several 'edge' statements
+        {{.cmpt_relations}}
+        }
+        ", .model_states = sep_values(names(model_states)),
+          .model_inputs = sep_values(get_inputs(model_states)),
+          .inputs = prepare_inputs(model_states),
+          .cmpt_relations =  paste0(cmpt_relations$result, collapse = " ")
+  )
+
+  test_grviz
+}
+
+visualize <- function(model) {
+  diagram <- generate_diagram_syntax(model)
+  DiagrammeR::grViz(diagram)
+}
 get_states_replacements <- function(model) {
   model_states <- AZRsim:::get_all_states(model)$stateODEs
   replacements <- AZRsim:::get_all_variables(model)$varformulas
@@ -23,38 +61,6 @@ get_states_replacements <- function(model) {
 
 
 
-cmpt_relations <- get_cmpt_relationships(model_states) %>%
-  mutate(result = paste0(from, "->", to, ";"))
-  paste0(cmpt_relations$result, collapse = " ")
-
-test_grviz <- infuser::infuse("
-  digraph test {
-
-      # several 'node' statements
-      node [shape = box,fontname = Helvetica]
-      {{.model_states}}
-
-      node [shape = circle,
-      fixedsize = true,
-      width = 0.9]
-      {{.model_inputs}}
-
-      node [style=invis, width=0.1, height=0.1]
-      empty;
-
-      # inputs
-      {{.inputs}}
-      # several 'edge' statements
-      {{.cmpt_relations}}
-      }
-      ", .model_states = sep_values(names(model_states)),
-        .model_inputs = sep_values(get_inputs(model_states)),
-        .inputs = prepare_inputs(model_states),
-        .cmpt_relations =  paste0(cmpt_relations$result, collapse = " ")
-)
-
-test_grviz
-grViz(test_grviz)
 
 # hand coded example
 grViz("
