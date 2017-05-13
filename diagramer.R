ce2b4f82547d60f79e92ee7e46d258d6fcb47db8
Devin Pastoor <devin.pastoor@gmail.com>
2017-05-12 20:11:17 -0400

    working on parsing relationships

--- diagramer.R
+++ diagramer.R
@@ -2,7 +2,9 @@ install.packages("DiagrammeR")
 library(DiagrammeR)
 
 
-get_replacement_vars <- function(model) {
+model_states <- AZRsim:::get_all_states(model)$stateODEs
+
+get_states_replacements <- function(model) {
   model_states <- AZRsim:::get_all_states(model)$stateODEs
   replacements <- AZRsim:::get_all_variables(model)$varformulas
   # remove input and outputs
@@ -18,6 +20,7 @@ get_replacement_vars <- function(model) {
   stringr::str_replace_all(model_states, inverted)
 }
 
+
 get_inputs <- function(model_states) {
   inputs <- unlist(stringr::str_extract_all(model_states, "input\\d+" ))
   return(unique(inputs))
@@ -28,6 +31,54 @@ sep_values <- function(.values, .sep = ";") {
 }
 
 model_states
+
+model_chunks <- chunk_ode(model_states)
+
+prepare_inputs <- function(model_states) {
+  model_inputs <- purrr::set_names(
+    stringr::str_extract_all(model_states, "input\\d+" ),
+    names(model_states)
+  )
+  paste0(purrr::flatten_chr(
+    purrr::map(names(model_inputs), ~ paste0(model_inputs[[.x]], " -> ", .x, ";"))
+  ), collapse = " ")
+}
+
+get_cmpt_relationships <- function(model_states) {
+  model_chunks <- chunk_ode(model_states)
+
+  positive_relationships <-
+    purrr::set_names(map(names(model_chunks), function(.x){
+      chunks <- model_chunks[[.x]]
+      positives <- which(names(chunks) == "p")
+      chunks <- chunks[positives]
+      to_remove <- which(str_detect(chunks, "input\\d+"))
+      if (length(to_remove)) {
+        chunks <- chunks[-to_remove]
+      }
+      return(chunks)
+  }), names(model_chunks))
+  negative_relationships <-
+      purrr::set_names(map(names(model_chunks), function(.x){
+      chunks <- model_chunks[[.x]]
+      negatives <- which(names(chunks) == "n")
+      chunks <- chunks[negatives]
+      to_remove <- which(str_detect(chunks, "input\\d+"))
+      if (length(to_remove)) {
+        chunks <- chunks[-to_remove]
+      }
+      return(chunks)
+  }), names(model_chunks))
+
+  map(positive_relationships, function(.relationship){
+    if (length(.relationship)) {
+      map(.relationship, function(.chunk) {
+
+      })
+    }
+
+  })
+}
 
 test_grviz <- infuser::infuse("
   digraph test {
@@ -43,12 +94,16 @@ test_grviz <- infuser::infuse("
 
       node [style=invis, width=0.1, height=0.1]
       empty;
+
+      # inputs
+      {{.inputs}}
       # several 'edge' statements
-      Ad -> Cc [label='Ka']; input1 -> Ad;
-      input2 -> Cc ; Cc -> empty [label='CL'];
       }
       ", .model_states = sep_values(names(model_states)),
-        .model_inputs = sep_values(get_inputs(model_states)))
+        .model_inputs = sep_values(get_inputs(model_states)),
+        .inputs = prepare_inputs(model_states),
+        .cmpt_relations = get_cmpt_relationships(model_states)
+)
 
 test_grviz
 grViz(test_grviz)
