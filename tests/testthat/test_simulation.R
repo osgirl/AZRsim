474f7aee118973e42bdf7b6cb5288029ecedba9d
Imad Ali <imadmali@gmail.com>
2017-06-09 16:56:38 -0400

    adding dosing table test

--- tests/testthat/test_simulation.R
+++ tests/testthat/test_simulation.R
@@ -61,3 +61,25 @@ test_that("create_model and deSolve return the same simulation for a SHO model",
   deSolve_sho <- deSolve::ode(y = yini, times = azr_sho$TIME, func = sho, parms = pars)
   expect_equal(azr_sho$y1, deSolve_sho[,2], tolerance = 1e-3)
 })
+
+test_that("a dosing table in simulate.azrmod and an event data frame in deSolve return the same simulation", {
+          model <- create_model(system.file("examples/one_cpt_dt.txt", package="AZRsim"))
+          dt <- data.frame("TIME" = seq(1,9, by = 1),
+                           "DOSE" = 40,
+                           "DURATION" = 0,
+                           "INPUT" = 1,
+                           "LAGTIME" = 0,
+                           stringsAsFactors = FALSE)
+          one_cpt_dt_simulation <- simulate(model, 10, dosingTable = dt)
+
+          one_cpt <- function(t, y, p) {
+            with(as.list(c(y,p)), {
+            dydt = -theta * y
+            return(list(dydt))
+            })
+          }
+          eventdf <- data.frame(var = "y", time = dt$TIME, value = dt$DOSE, method = "add")
+          deSolve_one_cpt <- deSolve::ode(y = c(y=0), times = one_cpt_dt_simulation$TIME,
+                                          func = one_cpt, parms = c(theta = 0.6), events = list(data = eventdf))
+          expect_equal(one_cpt_dt_simulation[,"y"], deSolve_one_cpt[,"y"], tol = 0.001)
+          })
